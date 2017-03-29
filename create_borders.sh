5d63c96c8da82832a2b4f170317a178c4ab3690f
walsh9 <walsh9@gmail.com>
2017-03-29 15:49:47 -0700

    add path parameter to border script

--- create_borders.sh
+++ create_borders.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+DEFAULT_PATH='./public/assets'
+BORDER_PATH=${1:-$DEFAULT_PATH}
 
 borders="2764 1f338 2b50 1f536 1f3b6 1f43e"
 
-mkdir -p ./public/assets/borders
-rm -f ./public/assets/borders/*.png
+mkdir -p $BORDER_PATH/borders
+rm -f $BORDER_PATH/borders/*.png
 
 for border in $borders
 do
@@ -18,16 +20,16 @@ do
   ./node_modules/emojione/assets/png/$border.png \
   ./node_modules/emojione/assets/png/$border.png \
   -mode Concatenate -tile 3x3 \
-  ./public/assets/borders/$border.png
+  $BORDER_PATH/borders/$border.png
 
   convert \
-  ./public/assets/borders/$border.png \
+  $BORDER_PATH/borders/$border.png \
   -monochrome -colors 2 \
-  ./public/assets/borders/$border.png
+  $BORDER_PATH/borders/$border.png
 done
 
 if command -v imageoptim >/dev/null 2>&1; then
-  imageoptim --directory ./public/assets/borders
+  imageoptim --directory $BORDER_PATH/borders
 else
   echo >&2 "Could not find imageoptim-cli. Skipping optimization"
 fi
